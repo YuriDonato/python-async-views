622e50e09474994fe8b93b12131b7ad242cc411f
YuriDonato <yuridonatosis@gmail.com>
2024-01-06 22:04:55 -0300

    a

--- asyncviews/urls.py
+++ asyncviews/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', views.async_view),
+    path('sync/', views.sync_view)
 ]
